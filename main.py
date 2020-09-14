778bb21253ebd2d62ed18dc24b2e0a3bae177c57
Tom Lesh <thomas.lesh@gmail.com>
2020-09-14 16:51:02 -0400

    better organization thanks to tz

--- main.py
+++ main.py
@@ -7,16 +7,14 @@ import mysql.connector
 
 
 def file_list(work_folder):
-    fileset = set('')
-    files = (file for file in os.listdir(work_folder)
-             if os.path.isfile(os.path.join(work_folder, file)))
-    for file in files:
-        fileset.add(file)
+    fileset = set()
+    fileset = set(f for f in os.listdir(work_folder)
+                  if os.path.isfile(os.path.join(work_folder, f)))
     return fileset
 
 
 def db_file_list(results):
-    fileset = set('')
+    fileset = set()
     logger.info('Connecting to the DB...')
     mydb = mysql.connector.connect(
         host=os.getenv("MYSQL_HOST"),
@@ -31,8 +29,7 @@ def db_file_list(results):
 
     myresult = mycursor.fetchall()
 
-    for x in myresult:
-        fileset.add(x[0])
+    fileset.update(x[0] for x in myresult)
 
     print("Database records returned: ", len(fileset))
 
@@ -40,12 +37,11 @@ def db_file_list(results):
 
 
 def s3_file_list(results):
-    fileset = set('')
+    fileset = set()
     logger.info('Connecting to S3...')
     response = client.Bucket(os.getenv('SPACES_BUCKET'))
     filelist = list(response.objects.all())
-    for f in filelist:
-        fileset.add(f.key)
+    fileset.update(f.key for f in filelist)
 
     print("S3 records returned: ", len(fileset))
 
@@ -59,38 +55,33 @@ def write_csv(filename, filelist):
             wr.writerow([row])
 
 
-load_dotenv(find_dotenv())
-
-logging.basicConfig(
+def main():
+    global logger, client
+    load_dotenv(find_dotenv())
+    logging.basicConfig(
         level=logging.INFO,
         format=f'%(asctime)s %(levelname)s %(message)s'
     )
-logger = logging.getLogger()
-logger.debug('Starting...')
+    logger = logging.getLogger()
+    logger.debug('Starting...')
+    folder = os.getenv("LOCAL_FOLDER")
+    session = boto3.session.Session()
+    client = session.resource('s3',
+                              region_name='nyc3',
+                              endpoint_url='https://nyc3.digitaloceanspaces.com',
+                              aws_access_key_id=os.getenv('SPACES_KEY'),
+                              aws_secret_access_key=os.getenv('SPACES_SECRET'))
+    max_results = 99999
+    folder_local = file_list(folder)
+    folder_db = db_file_list(max_results)
+    folder_s3 = s3_file_list(max_results)
+    diff_db = folder_db.difference(folder_s3)
+    diff_s3 = folder_s3.difference(folder_db)
+    write_csv("errors.csv", diff_db)
+    write_csv("not_in_db.csv", diff_s3)
+    print(f"{len(diff_db)} files are enabled in the database, but not found in the file system.")
+    for f in diff_db:
+        print(f)
 
 
-folder = os.getenv("LOCAL_FOLDER")
-
-session = boto3.session.Session()
-
-client = session.resource('s3',
-                        region_name='nyc3',
-                        endpoint_url='https://nyc3.digitaloceanspaces.com',
-                        aws_access_key_id=os.getenv('SPACES_KEY'),
-                        aws_secret_access_key=os.getenv('SPACES_SECRET'))
-
-max_results = 99999
-folder_local = (file_list(folder))
-folder_db = (db_file_list(max_results))
-folder_s3 = (s3_file_list(max_results))
-
-diff_db = folder_db.difference(folder_s3)
-diff_s3 = folder_s3.difference(folder_db)
-
-
-write_csv("errors.csv", diff_db)
-write_csv("not_in_db.csv", diff_s3)
-
-print(f"{len(diff_db)} files are enabled in the database, but not found in the file system.")
-for f in diff_db:
-    print(f)
+main()
